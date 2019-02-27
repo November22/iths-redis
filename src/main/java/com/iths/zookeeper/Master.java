ad7b554e055789d5b24492b87efad5d51a0f1020
sen.huang <sen.huang@yeepay.com>
2019-02-27 23:56:55 +0800

    zookeeper-master version0.2

--- src/main/java/com/iths/zookeeper/Master.java
+++ src/main/java/com/iths/zookeeper/Master.java
@@ -81,80 +81,144 @@ public class Master implements Watcher {
 
     public void startZk() throws IOException, InterruptedException {
         zooKeeper = new ZooKeeper(this.hosts,15000,this);
-//        countDownLatch.wait();
         System.out.println("连接成功");
     }
 
-    public void checkMaster() throws InterruptedException {
-//        while (true){
-//            Stat stat = new Stat();
-//            try {
-//                byte[] data = zooKeeper.getData(masterPath, false, stat);
-//                isLeader = new String(data).equals(serverId);
-//                return true;
-//            } catch (KeeperException.NoNodeException e ){
-//                return false;
-//            }catch (KeeperException e) {}
-//        }
-        zooKeeper.getData(masterPath,false,masterCheckCallback,null);
-    }
 
+    /**
+     * 获取管理权
+     * @throws InterruptedException
+     */
     public void runForMaster() throws InterruptedException {
-//        while (true){
-//            try {
-//                zooKeeper.create(masterPath, serverId.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-//                isLeader = true;
-//                break;
-//            } catch (KeeperException.NodeExistsException e) {
-//                isLeader = false;
-//                break;
-//            } catch (KeeperException e){}
-//            if (checkMaster()) break;
-//        }
         zooKeeper.create(masterPath,serverId.getBytes(),ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL,masterCreateCallback,null);
     }
 
-    AsyncCallback.DataCallback masterCheckCallback = new AsyncCallback.DataCallback() {
-        public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
-            switch (KeeperException.Code.get(rc)){
-                case CONNECTIONLOSS:
-                    try {
-                        checkMaster();
-                        return;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                case NONODE:
-                    try {
-                        runForMaster();
-                        return;
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-            }
-        }
-    };
-
-    AsyncCallback.StringCallback masterCreateCallback = new AsyncCallback.StringCallback() {
+    /**
+     * 创建管理节点回掉
+     */
+    private AsyncCallback.StringCallback masterCreateCallback = new AsyncCallback.StringCallback() {
         public void processResult(int i, String path, Object o, String name) {
             System.out.println("path["+path+"],name["+name+"]");
             switch (KeeperException.Code.get(i)){
-                case OK:
+                case OK://执行成功
                     isLeader = true;
                     break;
-                case CONNECTIONLOSS:
+                case CONNECTIONLOSS://连接丢失
                     try {
                         checkMaster();
                         break;
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
+                case NODEEXISTS://节点已存在
+                    masterExists();
+                    break;
                 default:
                     isLeader = false;
             }
         }
     };
+
+
+    /**
+     * 校验主节点是否存在
+     * @throws InterruptedException
+     */
+    public void checkMaster() throws InterruptedException {
+        zooKeeper.getData(masterPath,false,masterCheckCallback,null);
+    }
+    /**
+     * 校验节点是否存在取值回调
+     */
+    private AsyncCallback.DataCallback masterCheckCallback = new AsyncCallback.DataCallback() {
+        public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
+            switch (KeeperException.Code.get(rc)){
+                case CONNECTIONLOSS://连接丢失
+                    try {
+                        checkMaster();
+                        return;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                case NONODE://节点不存在
+                    try {
+                        runForMaster();
+                        return;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                case OK://获取数据成功
+                    String dataStr = new String(data);
+                    if(serverId.equals(dataStr)){
+                        isLeader = true;
+                    }else{
+                        masterExists();
+                    }
+                default:
+                    try {
+                        checkMaster();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+            }
+        }
+    };
+
+    /**
+     * 校验主节点存在，设置监听器
+     */
+    private void masterExists(){
+        zooKeeper.exists("/master",masterExistsWatcher,masterExistsCallback,null);
+    }
+
+    /**
+     * 主节点验证存在的异步回调
+     */
+    private AsyncCallback.StatCallback masterExistsCallback = new AsyncCallback.StatCallback() {
+        public void processResult(int code, String path, Object ctx, Stat stat) {
+            switch (KeeperException.Code.get(code)){
+                case CONNECTIONLOSS://连接丢失，重新校验
+                    masterExists();
+                    break;
+                case OK://请求成功
+                    if(stat == null){
+                        //节点不存在,竞争成为主节点，中间情况下，获得主节点的服务器丢失
+                        try {
+                            runForMaster();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    //else 节点存在，设置监听器完成
+                    break;
+                default://重新校验与设置监听器
+                    try {
+                        checkMaster();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+            }
+        }
+    };
+    /**
+     * 设置对/master节点的监视
+     */
+    private Watcher masterExistsWatcher = new Watcher() {
+        public void process(WatchedEvent watchedEvent) {
+            if(watchedEvent.getType() == Event.EventType.NodeDeleted && "/master".equals(watchedEvent.getPath())){
+                try {
+                    runForMaster();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+
+
+
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Master master = new Master("127.0.0.1:2181");
--- src/test/java/com/iths/zookeeper/ZnodeTest.java
+++ src/test/java/com/iths/zookeeper/ZnodeTest.java
@@ -1,11 +1,13 @@
 package com.iths.zookeeper;
 
+import org.apache.zookeeper.AsyncCallback;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,14 +22,13 @@ public class ZnodeTest implements Watcher  {
 
     private ZooKeeper zooKeeper;
 
-    public ZnodeTest(){
-        System.out.println("示例话");
-    }
 
     private CountDownLatch countDownLatch = new CountDownLatch(1);//同步计数器
 
     public void process(WatchedEvent watchedEvent) {
-        System.out.println("event.state["+watchedEvent.getState()+"]");
+        System.out.println("event.state["+watchedEvent.getState()+"],"+
+                "event.type["+watchedEvent.getType()+"],"+
+                "event.path["+watchedEvent.getPath()+"]");
         //连接事件
         if(watchedEvent.getState() == Watcher.Event.KeeperState.SyncConnected){
             countDownLatch.countDown();//计数器减一
@@ -36,7 +37,6 @@ public class ZnodeTest implements Watcher  {
 
     @Before
     public void init() throws IOException, InterruptedException {
-        System.out.println("创建对象");
         //这个创建事件是异步的
         zooKeeper = new ZooKeeper("127.0.0.1:2181",30000,this);
         //需要计数器减为0才会执行
@@ -55,6 +55,24 @@ public class ZnodeTest implements Watcher  {
 
         zooKeeper.close();
         System.out.println("客户端关闭");
+        Thread.sleep(10*60*1000);
+    }
+
+    @Test
+    public void testWatcher() throws KeeperException, InterruptedException {
+        Watcher watcher = new Watcher() {
+            public void process(WatchedEvent watchedEvent) {
+                System.out.println("my.watcher.vent.state["+watchedEvent.getState()+"],"+
+                        "event.type["+watchedEvent.getType()+"],"+
+                        "event.path["+watchedEvent.getPath()+"]");
+            }
+        };
+        zooKeeper.exists("/watch1",watcher,new AsyncCallback.StatCallback(){
+            public void processResult(int code, String path, Object ctx, Stat stat) {
+                System.out.println("exists检查异步回调");
+            }
+        },null);
+
         Thread.sleep(10*60*1000);
     }
 }
